a345f4ba259e31c2a912fbaf2bcd1bc5168bbba1
root <root@ip-172-31-27-216.ap-northeast-1.compute.internal>
2016-06-04 04:14:17 -0400

    Actions added, not fully need getAction dispatcher

--- routes/routes.py
+++ routes/routes.py
@@ -5,6 +5,47 @@ import tornado.gen
 from tornado.web import  RequestHandler
 from models.models import User,Task
 
+
+class Action():
+
+    def __init__(self,handler,message = ''):
+        self._app = handler.application
+        self._message = message
+
+    @property
+    def application(self):
+        return self._app
+
+    @property
+    def message(self):
+        return self._message
+
+    def do(self):
+        pass
+
+class startAction(Action):
+
+    @tornado.gen.coroutine
+    def do(self):
+        user = User()
+        user.chat_id = ''.join(filter(str.isdigit,str(self.message['message']['chat']['id'])))
+        
+        conterQuery = { 
+                        'chat_id' : self.message['message']['chat']['id'],
+                        'text'    : ''
+                      }
+
+        cursor = yield self.application.db.execute(user.selectThis())
+        result = cursor.fetchone()
+        logging.debug("gotted result from db is %s " % str(result))
+        if result != None:
+            conterQuery['text'] = 'You are already registered!!! Then task'
+        else:
+            self.application.db.execute(user.insertThis())
+            conterQuery['text'] = 'You will be registered in system, task will be sended in seconds!!!'
+        return conterQuery
+
+
 class StartHandler(tornado.web.RequestHandler):
     
     @property
@@ -13,19 +54,12 @@ class StartHandler(tornado.web.RequestHandler):
 
     @tornado.gen.coroutine
     def post(self):
-        user = User()
         message = tornado.escape.json_decode(self.request.body)
-        conterQuery = { 'chat_id' : message['message']['chat']['id']}
         logging.debug("got request from telegram server as %s " % message)
-        user.chat_id = ''.join(filter(str.isdigit,str(message['message']['chat']['id'])))
-        cursor = yield self.db.execute(user.selectThis())
-        result = cursor.fetchone()
-        logging.debug("gotted result from db is %s " % str(result))
-        if result != None:
-            conterQuery['text'] = 'You are already registered!!! Then task'
-        else:
-            self.db.execute(user.insertThis())
-            conterQuery['text'] = 'You will be registered in system, task will be sended in seconds!!!'
+
+        action = startAction(self, message)
+        conterQuery = yield action.do() 
+
         responseConter = yield self.application.sendMessage(conterQuery)
         logging.debug("recieved answer from tlgServer is %s" % responseConter)
         self.finish() 
@@ -36,4 +70,6 @@ class InitHandler(tornado.web.RequestHandler):
         message = tornado.escape.json_decode(self.request.body)
         logging.debug(" nswer will be sended to tlgServer %s" % message)
         self.finish()
-    
+   
+
+
